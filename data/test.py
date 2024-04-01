296ad877c3a5da457701f78d861baa592f458e6e
NaomiH42 <naomi.hasalova@gmail.com>
2024-04-02 00:45:19 +0200

    it doesn't really work lol, also emoji_37 doesn't exist

--- data/test.py
+++ data/test.py
@@ -6,7 +6,7 @@ import time
 def checkForEmoji(img, emoji, X, Y):
     roi = img[Y:Y+50, X:X+50]
     diff = cv.subtract(roi, emoji)
-    diff = diff.astype(np.uint8)
+    # diff = diff.astype(np.uint8)
     # print(np.count_nonzero(diff))
     if np.count_nonzero(diff) < 3100:
         # print(diff)
@@ -27,29 +27,35 @@ clone = img.copy()
 
 height, width, depth = img.shape
 
-
-
-
-for i in range (205, height - 50):
-    for j in range (0, width - 50):
-        if checkForEmoji(img, emoji, j, i) == True:
-            print(j)
-            print(' ')
-            print(i)
-            cv.rectangle(img, (j, i), (j + 50, i + 50), color=(0,255,0))
-            cv.imshow('test', img)
-            cv.waitKey(0)
-            exit()
-        cv.rectangle(img, (j, i), (j + 50, i + 50), color=(0,0,255))
-        cv.imshow('test', img)
-        key = cv.waitKey(1)
-        if key == 27:#if ESC is pressed, exit loop
-            cv.destroyAllWindows()
-            exit()
-        img = clone.copy()
-        # print(img[i][j])
+for k in range(0, 100):
+    path = "./dataset/emoji_" + str(k) + ".jpg"
+    img = cv.imread(path)
+    for i in range (205, height - 50):
+        for j in range (0, width - 50):
+            check = checkForEmoji(img, emoji, j, i)
+            if check == True:
+                print("Emoji ", end="")
+                print(k, end=": ")
+                print(j, end=" ")
+                print(i)
+                break
+        if check == True:
+            check = False
+            break
+                # cv.rectangle(img, (j, i), (j + 50, i + 50), color=(0,255,0))
+                # cv.imshow('test', img)
+                # cv.waitKey(0)
+                # exit()
+            # cv.rectangle(img, (j, i), (j + 50, i + 50), color=(0,0,255))
+            # cv.imshow('test', img)
+            # key = cv.waitKey(1)
+            # if key == 27:#if ESC is pressed, exit loop
+            #     cv.destroyAllWindows()
+            #     exit()
+            # img = clone.copy()
+            # print(img[i][j])
 
 # roi = img[284:334,437:487]
-cv.imshow('test', roi)
+# cv.imshow('test', roi)
 
-cv.waitKey(0)
+# cv.waitKey(0)
