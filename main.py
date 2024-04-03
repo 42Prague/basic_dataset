9e4d1d063709676c8a8e45f1086dade34bac8787
pibouill <pibouill@student.42prague.com>
2024-04-03 19:27:20 +0200

    working kinda but without the scanning part

--- main.py
+++ main.py
@@ -6,7 +6,7 @@
 #    By: pibouill <pibouill@student.42prague.c      +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/04/02 12:32:21 by pibouill          #+#    #+#              #
-#    Updated: 2024/04/02 18:19:35 by pibouill         ###   ########.fr        #
+#    Updated: 2024/04/03 17:05:16 by pibouill         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -19,36 +19,37 @@ if len(sys.argv) != 2:
     print("Usage: python3 <file>.py <img>.jpg")
     sys.exit(1)
 
+# Gets the jpg from cli argument
 image_path = sys.argv[1]
 
 # Reads the image. 2 flag for greyscale
-#img = cv.imread(image_path, 2)
-#emoji_template = cv.imread("template_emoji.jpg", 2)
-#w, h = emoji_template.shape[::-1]
-#
-#print(w , h)
-#
-#res = cv.matchTemplate(img, emoji_template, 5)
-#max_loc = cv.minMaxLoc(res)
-#
-#top_left = max_loc
-#bottom_right = (top_left[0] + w, top_left[1] + h)
-#
-#print(top_left)
-#print(bottom_right)
-#
-#cv.rectangle(img,top_left, bottom_right, 255, 2)
-#
-#print(top_left)
-#print(bottom_right)
+img = cv.imread(image_path, 2)
+# Reads the emoji template
+emoji_template = cv.imread("template_emoji.jpg", 2)
 
-#plt.subplot(121),plt.imshow(res,cmap = 'gray')
-#plt.title('Matching Result'), plt.xticks([]), plt.yticks([])
-#plt.subplot(122),plt.imshow(img,cmap = 'gray')
-#plt.title('Detected Point'), plt.xticks([]), plt.yticks([])
+method = cv.TM_CCOEFF_NORMED
+# Apply the match finding method to the img
+result = cv.matchTemplate(emoji_template, img, method)
 
-#plt.show()
+# Slice the emoji template to get the width and height of the square
+w, h = emoji_template.shape[:2]
 
-# Display the image
-#cv2.imshow("Binary", bw_img)
+min_val, max_val, min_loc, max_loc = cv.minMaxLoc(result)
 
+
+
+top_left = max_loc
+bottom_right = (top_left[0] + w, top_left[1] + h) 
+
+
+rectangle_color = (0, 0, 255)
+rectangle_thickness = 1
+
+img = cv.cvtColor(img, cv.COLOR_GRAY2BGR)
+cv.rectangle(img, top_left, bottom_right, rectangle_color, rectangle_thickness)
+
+
+cv.imshow('output', img)
+cv.waitKey(0)
+
+print("Found emoji at :",max_loc)
